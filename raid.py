b370a86b939b59ae0e43b1db087bcb6218c9f899
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-05 12:38:23 +0530

    Update raid.py

--- raid.py
+++ raid.py
@@ -328,6 +328,45 @@ async def _(event):
             reply_to=event.message.id,
         )
 
+@bot.on(admin_cmd(pattern="raid(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern="raid(?: |$)(.*)", allow_sudo=True))
+async def spam(e):  
+    if ABUSE == "ON":
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        legend = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        smex = await e.get_reply_message()
+        if len(legend) == 2:
+            message = str(legend[1])
+            print(message)
+            a = await e.client.get_entity(message)
+            g = a.id
+            c = a.first_name
+            username = f"[{c}](tg://user?id={g})"
+            counter = int(legend[0])
+            for _ in range(counter):
+                reply = random.choice(RAID)
+                caption = f"{username} {reply}"
+                async with e.client.action(e.chat_id, "typing"):
+                    await e.client.send_message(e.chat_id, caption)
+                    await asyncio.sleep(0.3)
+        elif e.reply_to_msg_id:             
+            a = await e.get_reply_message()
+            b = await e.client.get_entity(a.sender_id)
+            g = b.id
+            c = b.first_name
+            counter = int(legend[0])
+            username = f"[{c}](tg://user?id={g})"
+            for _ in range(counter):
+                reply = random.choice(RAID)
+                caption = f"{username} {reply}"
+                async with e.client.action(e.chat_id, "typing"):
+                    await e.client.send_message(e.chat_id, caption)
+                    await asyncio.sleep(0.3)
+    else:
+        await e.reply(usage, parse_mode=None, link_preview=None )
+
+
 @bot.on(admin_cmd(pattern="replyraid(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="replyraid(?: |$)(.*)", allow_sudo=True))
 async def _(event):
